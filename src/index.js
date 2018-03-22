06fd85f4c9c14e99f8feb368f6dacde1918b5d4d
davidtangjw <davidtangjw@pptv.com>
2018-03-22 14:09:43 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ var Runner = require('./runner');
 window.onload = function () {
 
 	
-	/* window.onError = function() {
-		if (vConsole.$dom) {
-			this.$dom.style.display = 'block';
-		}
-	}
- */
 	var vConsole = new VConsole({
 		onReady: function() {
 			//this.$dom.style.display = 'none';
