c1ff05e5c65e26f44f0e9de79bfb7cd62ab533b5
davidtangjw <davidtangjw@pptv.com>
2018-03-22 14:08:02 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ var Runner = require('./runner');
 window.onload = function () {
 
 	
-	window.onError = function() {
+	/* window.onError = function() {
 		if (vConsole.$dom) {
 			this.$dom.style.display = 'block';
 		}
 	}
-
+ */
 	var vConsole = new VConsole({
 		onReady: function() {
-			this.$dom.style.display = 'none';
+			//this.$dom.style.display = 'none';
 			var runner = new Runner('#game-containter');
-			tt = rr;
 		}
 	});
 	
